90382df16526e4f5317db68e1e32703516a54daf
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-05-16 14:30:29 +0100

    Make sure the pyenv hooks don't error out

--- etc/pyenv.d/version-name/virtualenv-ext.bash
+++ etc/pyenv.d/version-name/virtualenv-ext.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -z "$(printenv PYENV_VERSION)" ]; then
-  PYENV_VIRTUAL_ENV="$(pyenv-virtualenv-name)"
+  PYENV_VIRTUAL_ENV="$(pyenv-virtualenv-name || true)"
   if [ -n "$PYENV_VIRTUAL_ENV" ]; then
     PYENV_VERSION="$PYENV_VIRTUAL_ENV"
   fi
